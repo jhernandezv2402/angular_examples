b48ee965affd1fda178d5d1af8dc45974ee89a45
Jorge Salgado Win <keymanky@hotmail.com>
2015-05-12 17:42:31 -0500

    Jorgito

---
+++ script.js
@@ -0,0 +1,13 @@
+// angular.module('simpleController',[] )
+// 	.controller('simpleController2', function ($scope)
+// {
+//   // Initialize the model variables
+//   $scope.firstName = "John";
+//   $scope.lastName = "Doe";
+
+//   // Define utility functions
+//   $scope.getFullName = function ()
+//   {
+//     return $scope.firstName + " " + $scope.lastName;
+//   };
+// });
